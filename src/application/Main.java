4016370fe76eefa21d54f9f6e0798c25fe31164f
Wanderson Fagundes <wandersonfagundesramos@gmail.com>
2021-04-20 14:39:39 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,8 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -20,7 +22,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Aplicação JavaFX de Modelo");
 			primaryStage.show();
@@ -28,6 +30,10 @@ public class Main extends Application {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene () {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {

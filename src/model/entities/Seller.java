8b0f689f286a4ea9ee8883fb2ee4baa726e35dbb
Wanderson Fagundes <wandersonfagundesramos@gmail.com>
2021-04-22 18:51:31 -0300

    DepartmentForm (dialog) design

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -105,4 +105,7 @@ public class Seller implements Serializable {
 		return "Seller [id = " + id + ", name = " + name + ", email = " + email + ", birthDate = " + birthDate + ", baseSalary = "
 				+ baseSalary + ", department = " + department + "]";
 	}
+
+	public void setBirthDate(java.sql.Date date) {
+	}
 }

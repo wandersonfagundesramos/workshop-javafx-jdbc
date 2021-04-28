c19af7ffd31290b31a8e6666df10edd167ea09e5
Wanderson Fagundes <wandersonfagundesramos@gmail.com>
2021-04-27 23:35:25 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -18,6 +21,8 @@ import model.entities.Department;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable  {
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 	
 	private DepartmentService service;
 	
@@ -46,6 +51,10 @@ public class DepartmentFormController implements Initializable  {
 		this.entity = entity;
 	}
 	
+	public void subscribeDataChangeListener (DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
 	@FXML
 	public void onBtSaveAction (ActionEvent event) {
 		
@@ -59,6 +68,7 @@ public class DepartmentFormController implements Initializable  {
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		}
 		catch (DbException e) {
@@ -66,6 +76,13 @@ public class DepartmentFormController implements Initializable  {
 		}
 	}
 	
+	private void notifyDataChangeListeners () {
+		
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+	}
+
 	private Department getFormData() {
 		Department obj = new Department();
 		
